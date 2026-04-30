ae12bacf79fedd22e7a2bc1f1c161e0875d2620e
sachinaj3333-dotcom <sachinaj3333@gmail.com>
2026-04-30 13:52:30 +0530

    authentication code completed.

--- src/components/auth/Signupform.jsx
+++ src/components/auth/Signupform.jsx
@@ -36,7 +36,7 @@ const SignupForm = () => {
             <span className={`text-danger ${Styles.error}`}>{errors.userEmail}</span>
           )}
         </div> 
-        <div className="col-lg-6 mb-2">
+        <div className="col-lg-6 mb-3">
           <label className={`form-label ${Styles.formlabel}`}>Password</label>
           <span className={`${Styles.password_logo}`}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4M4.5 7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7zM8 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3" />
@@ -46,7 +46,7 @@ const SignupForm = () => {
             <span className={`text-danger ${Styles.error}`}>{errors.userPass}</span>
           )}
         </div>
-        <div className="col-lg-6 mb-2">
+        <div className="col-lg-6 mb-3">
           <label className={`form-label ${Styles.formlabel}`}>Confirm password</label>
           <span className={`${Styles.confirmpass_logo}`}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4M4.5 7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7zM8 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3" />
