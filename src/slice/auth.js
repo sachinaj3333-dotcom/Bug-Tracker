c33b7e856ddaedbac7c3c6efac4fb1b6cbde0b58
sachinaj3333-dotcom <sachinaj3333@gmail.com>
2026-04-30 13:00:59 +0530

    signup form and form validation created.

--- src/slice/auth.js
+++ src/slice/auth.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { signInUser, signUpUser } from "../services/auth-api"
 
 
 
@@ -10,3 +12,46 @@ const initialState = {
     status: null,
 }
 
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            // Sign in
+            .addCase(signInUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(signInUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+                state.status = action.payload.status;
+                state.message = action.payload.message;
+            })
+            .addCase(signInUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.response.data.message || "Sign in failed";
+                state.message = action.payload.response.data.message || "Try again";
+            })
+
+            // Sign up
+            .addCase(signUpUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(signUpUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload.user;
+                state.message = action.payload.message;
+            })
+            .addCase(signUpUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.error || "Sign up failed";
+                state.message = action.payload.message || "Try again";
+            })
+    }
+})
+
+export default authSlice.reducer;
